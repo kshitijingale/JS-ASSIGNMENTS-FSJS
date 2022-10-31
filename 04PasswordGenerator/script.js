9d39858930ec22f21a9c6ef307455814b0070868
Kshitij Ingale <kshitijingale2@gmail.com>
2022-10-31 13:34:03 +0530

    Password Generator Completed

--- 04PasswordGenerator/script.js
+++ 04PasswordGenerator/script.js
@@ -15,25 +15,66 @@ const randomFunc = {
 }
 
 clipboardEl.addEventListener('click', () => {
-    
+    navigator.clipboard.writeText(resultEl.innerText);
+    alert("Password Copied.....");
 })
 
 generateEl.addEventListener('click', () => {
-    
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, lengthEl.value);
 })
 
 function generatePassword(lower, upper, number, symbol, length) {
-    
+    let password = "";
+    const charTypes = lower + upper + symbol + number;
+    if (charTypes == 0) {
+        return;
+    }
+
+    while (password.length <= length) {
+        if (lower) {
+            password += randomFunc.lower();
+        }
+        if (upper) {
+            password += randomFunc.upper();
+        }
+        if (number) {
+            password += randomFunc.number();
+        }
+        if (symbol) {
+            password += randomFunc.symbol();
+        }
+    }
+    return password.slice(0, length);
 }
 
 function getRandomLower() {
+    const lowerChar = "abcdefghijklmnopqrstuvwxyz";
+    const randomNum = Math.floor(Math.random() * 26);
+
+    return lowerChar[randomNum];
 }
 
 function getRandomUpper() {
+    const upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const randomNum = Math.floor(Math.random() * 26);
+
+    return upperChar[randomNum];
 }
 
 function getRandomNumber() {
+    const nums = "0123456789";
+    const randomNum = Math.floor(Math.random() * 10);
+
+    return nums[randomNum];
 }
 
 function getRandomSymbol() {
+    const symbolsList = "~`!@#$%^&*()_-+={[}]|\:;'<,>.?/";
+    const randomNum = Math.floor(Math.random() * symbolsList.length);
+
+    return symbolsList[randomNum];
 }
