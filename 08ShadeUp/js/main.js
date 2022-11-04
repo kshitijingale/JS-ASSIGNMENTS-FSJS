bc7ed64290a8829826218492f78ca1760c33114a
Kshitij Ingale <kshitijingale2@gmail.com>
2022-11-04 10:36:04 +0530

    ShadeUp

--- 08ShadeUp/js/main.js
+++ 08ShadeUp/js/main.js
@@ -1,28 +1,55 @@
 function randompickerbtn() {
-  
+    const randomPicker = document.getElementById('randomPicker');
+    const randomValuePara = document.getElementById('randomValuePara');
+    let randomColor = "#";
+    const hexCode = "ABCDEF0123456789";
+    for (let i = 0; i < 6; i++) {
+        const randomNum = Math.floor(Math.random() * 16);
+        randomColor += hexCode[randomNum];
+    }
+
+    randomPicker.style.background = randomColor;
+    randomValuePara.innerText = randomColor;
+
 }
 
 function hexFindbtn() {
+    const hexInput = document.getElementById('hexInput');
+    const colorBox = document.getElementById('color-box');
+
+    colorBox.style.backgroundColor = hexInput.value;
 }
 
 function RGBFindbtn() {
-  
+    const r = document.getElementById('RInput')
+    const g = document.getElementById('GInput')
+    const b = document.getElementById('BInput')
+    const rgbColorBox = document.getElementById('rgb-color-box');
+
+    rgbColorBox.style.backgroundColor = `rgb(${r.value},${g.value},${b.value})`;
+
 }
 
 function RangeSelector() {
-  
+    const r = document.getElementById('RrInput');
+    const g = document.getElementById('GrInput');
+    const b = document.getElementById('BrInput');
+    const colorBox = document.getElementById('range-color-box');
+
+    colorBox.style.backgroundColor = `rgb(${r.value},${g.value},${b.value})`
+
 }
 
 // hex to rgb converter
 
 String.prototype.convertToRGB = function () {
-  
+
 };
 
 function hexConvbtn() {
-  
+
 }
 
 function rgbConvbtn() {
-  
+
 }
