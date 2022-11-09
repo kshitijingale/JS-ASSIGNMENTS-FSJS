58b11bf363b413fdc4b122bea56c2fd2108500d0
Kshitij Ingale <kshitijingale2@gmail.com>
2022-11-09 12:00:27 +0530

    Calculator Completed

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,14 +1,26 @@
+let equationStr = "";
 
-function insert(num){
-  }
-  
-  function equals(){
-    
-      
-  }
-  
-  function clean(){
-  }
-  
-  function back(){
-  }
+function insert(num) {
+  equationStr += num;
+  display();
+}
+
+function equals() {
+  const answer = eval(equationStr);
+  equationStr = answer.toString();
+  display();
+}
+
+function clean() {
+  equationStr = "";
+  display();
+}
+
+function back() {
+  equationStr = equationStr.slice(0, -1);
+  display();
+}
+
+function display() {
+  document.querySelector('.textview').value = equationStr;
+}
